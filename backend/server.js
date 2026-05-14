36f4954857f7ef4da9d13f535c2d77d13af2b1de
Charanreddy <charanreddysama990@gmail.com>
2026-05-14 21:08:32 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ config(); //process.env
 const app = exp();
 //use cors middleware
 app.use(
-  cors({ origin: ["http://localhost:5173", "http://localhost:5174"], credentials: true })
+  cors({ origin: [ "https://blog-app-capstone.vercel.app", "http://localhost:5173"], credentials: true })
 );
 //add body parser middleware
 app.use(exp.json());
